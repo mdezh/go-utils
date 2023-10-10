92763dd807dc7f4a2d939e1ae27e60e3368f3de8
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-10-10 20:52:35 +0300

    add WaitRand

--- pkg/wait/wait.go
+++ pkg/wait/wait.go
@@ -2,10 +2,16 @@ package wait
 
 import (
 	"context"
+	"errors"
+	"math/rand"
 	"time"
 )
 
 func Wait(ctx context.Context, d time.Duration) error {
+	if d <= 0 {
+		return nil
+	}
+
 	timer := time.NewTimer(d)
 	defer timer.Stop()
 
@@ -32,4 +38,14 @@ func WaitCh[T any](ctx context.Context, ch <-chan T) error {
 	case <-ch:
 		return nil
 	}
+}
+
+func WaitRand(ctx context.Context, minD, maxD time.Duration) error {
+	if minD > maxD {
+		return errors.New("failed to wait: min time duration greater than max time duration")
+	}
+
+	ms := minD.Milliseconds() + int64(rand.Intn(int(maxD.Milliseconds()-minD.Milliseconds())+1))
+
+	return Wait(ctx, time.Duration(ms*int64(time.Millisecond)))
 }
